3f97f857118298890ffa93e681fe6a9f16d5bfd4
Heinz N. Gies <heinz@licenser.net>
2012-12-30 06:20:31 +0100

    Added version report.

--- apps/howl/src/howl_tcp_protocol.erl
+++ apps/howl/src/howl_tcp_protocol.erl
@@ -4,6 +4,7 @@
 
 -ignore_xref([init/1, message/2]).
 
+-include("howl_version.hrl").
 
 init([]) ->
     {ok, stateless}.
@@ -15,6 +16,9 @@ init([]) ->
 message({msg, Channel, Msg}, State) ->
     howl:send(Channel, Msg),
     {noreply, State};
+
+message(version, State) ->
+    {reply, ?VERSION, State};
 
 message(Oops, State) ->
     io:format("oops: ~p~n", [Oops]),
