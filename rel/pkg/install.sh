ff915856fdf649277623c14b0d973cdf7fc96dfa
Heinz N. Gies <heinz@licenser.net>
2015-08-10 00:15:09 +0200

    updated rebar3

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -7,13 +7,6 @@ SED=/usr/bin/sed
 
 case $2 in
     PRE-INSTALL)
-        #if grep '^Image: base64 1[34].[1234].*$' /etc/product
-        #then
-        #    echo "Image version supported"
-        #else
-        #    echo "This image version is not supported please use the base64 13.2.1 image or later."
-        #    exit 1
-        #fi
         if grep "^$GROUP:" /etc/group > /dev/null 2>&1
         then
             echo "Group already exists, skipping creation."
@@ -48,10 +41,10 @@ case $2 in
             cp ${CONFFILE}.example ${CONFFILE}
             $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
         else
-            echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
-            /opt/local/fifo-howl/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
-                mv ${CONFFILE} ${CONFFILE}.old &&
-                mv ${CONFFILE}.new ${CONFFILE}
+            # echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
+            # /opt/local/fifo-howl/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
+            #    mv ${CONFFILE} ${CONFFILE}.old &&
+            #    mv ${CONFFILE}.new ${CONFFILE}
         fi
         ;;
 esac
