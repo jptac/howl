ece175ad3cb338d51233f7c539e44161aa9d7825
Heinz N. Gies <heinz@licenser.net>
2015-08-19 15:11:10 +0200

    updated install file.

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -40,7 +40,7 @@ case $2 in
             echo "Creating new configuration from example file."
             cp ${CONFFILE}.example ${CONFFILE}
             $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
-        else
+        # else
             # echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
             # /opt/local/fifo-howl/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
             #    mv ${CONFFILE} ${CONFFILE}.old &&
