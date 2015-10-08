beedb5f594910e76f913a250d5ad4b39766f9c1e
Heinz N. Gies <heinz@licenser.net>
2015-10-08 10:40:03 +0200

    Fully remove config update scirpt

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -40,9 +40,9 @@ case $2 in
             echo "Creating new configuration from example file."
             cp ${CONFFILE}.example ${CONFFILE}
             $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
-        # else
-            # echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
-            # /opt/local/fifo-howl/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
+        else
+            echo "Please make sure you update your config according to the update manual!"
+            #/opt/local/fifo-sniffle/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
             #    mv ${CONFFILE} ${CONFFILE}.old &&
             #    mv ${CONFFILE}.new ${CONFFILE}
         fi
