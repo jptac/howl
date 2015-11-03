bd0b3f597b75328be523a556ceb2a655b2769d7f
Heinz N. Gies <heinz@licenser.net>
2015-11-03 18:30:45 +0100

    Add self signed SSL cert generation

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -5,6 +5,11 @@ GROUP=$USER
 AWK=/usr/bin/awk
 SED=/usr/bin/sed
 
+DOMAIN="project-fifo.net"
+CERTDIR="/var/db/fifo"
+CERTPREFIX="fifo"
+DAYS=3650
+
 case $2 in
     PRE-INSTALL)
         if grep "^$GROUP:" /etc/group > /dev/null 2>&1
@@ -28,6 +33,62 @@ case $2 in
         chown -R howl:howl /var/db/howl
         mkdir -p /var/log/howl/sasl
         chown -R howl:howl /var/log/howl
+
+        ## Certificate creation:
+        if [ ! -d $CERTDIR ]
+        then
+            echo "Generating self signed SSL cert in $CERTDIR"
+            #echo Trying to guess network configuration ...
+
+            if ifconfig net1 > /dev/null 2>&1
+            then
+                IP=`ifconfig net1 | grep inet | $AWK '{print $2}'`
+            else
+                IP=`ifconfig net0 | grep inet | $AWK '{print $2}'`
+            fi
+            SUBJ="
+C=AU
+ST=Victoria
+O=Company
+localityName=Melbourne
+commonName=$IP
+organizationalUnitName=None
+emailAddress=blah@blah.com
+"
+
+
+            export PASSPHRASE=$(head -c 128 /dev/random  | uuencode - | grep -v "^end" | tr "\n" "d")
+            echo "Creating certificates"
+            mkdir -p $CERTDIR
+
+            openssl genrsa -des3 -out $CERTDIR/$CERTPREFIX.key -passout env:PASSPHRASE 2048
+            fail_if_error $?
+
+            openssl req \
+                    -new \
+                    -batch \
+                    -subj "$(echo -n "$SUBJ" | tr "\n" "/")" \
+                    -key $CERTDIR/$CERTPREFIX.key \
+                    -out $CERTDIR/$CERTPREFIX.csr \
+                    -passin env:PASSPHRASE
+            fail_if_error $?
+
+            cp $CERTDIR/$CERTPREFIX.key $CERTDIR/$CERTPREFIX.key.org
+            fail_if_error $?
+
+            openssl rsa -in $CERTDIR/$CERTPREFIX.key.org -out $CERTDIR/$CERTPREFIX.key -passin env:PASSPHRASE
+            fail_if_error $?
+
+            openssl x509 -req -days $DAYS -in $CERTDIR/$CERTPREFIX.csr -signkey $CERTDIR/$CERTPREFIX.key -out $CERTDIR/$CERTPREFIX.crt
+            fail_if_error $?
+
+            cat $CERTDIR/$CERTPREFIX.key $CERTDIR/$CERTPREFIX.crt > $CERTDIR/$CERTPREFIX.pem
+
+            chgrp -R $GROUP $CERTDIR
+
+            unset PASSPHRASE
+        fi
+
         ;;
     POST-INSTALL)
         svccfg import /opt/local/fifo-howl/share/howl.xml
