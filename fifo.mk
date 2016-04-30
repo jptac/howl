7ccf2f79534cd48dea2c9422fb33f0f5d431be19
Heinz N. Gies <heinz@licenser.net>
2016-04-30 17:03:43 +0200

    Update fifo.mk

--- fifo.mk
+++ fifo.mk
@@ -10,7 +10,7 @@ compile: $(REBAR) .git/hooks/pre-commit
 .git/hooks/pre-commit: hooks/pre-commit
 	cp hooks/pre-commit .git/hooks
 
-pre-commit: test-scripts lint xref dialyzer test vsn-test
+pre-commit: test-scripts test-vsn lint xref dialyzer test 
 
 dialyzer: $(REBAR)
 	$(REBAR) dialyzer
@@ -63,7 +63,7 @@ vsn:
 	@echo "## rel/vars.config"
 	@echo "$(VARS_VSN)"
 
-vsn-test:
+test-vsn:
 	@echo "Testing against package version: $(PKG_VSN)"
 	@[ "$(PKG_VSN)" == "$(APP_VSN)" ]   && echo " - App version ok:   $(APP_VSN)"   || (echo "App version out of date" && false)
 	@[ "$(PKG_VSN)" == "$(REBAR_VSN)" ] && echo " - Rebar version ok: $(REBAR_VSN)" || (echo "Rebar version out of date" && false)
